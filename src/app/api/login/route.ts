569c0c4286b2045380f206163c390b6c8ce63c32
Kiran Krishna N <kiran.krishna@nucash.money>
2024-10-27 02:50:35 +0530

    feat: add login api

---
+++ src/app/api/login/route.ts
@@ -0,0 +1,49 @@
+import { SECRET } from "@/app/constants"
+import { ErrorHandler } from "@/app/utils/errorhandler"
+import { dbConnect } from "@/lib/mongodb"
+import User from "@/models/User"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import { NextRequest, NextResponse } from "next/server"
+
+export async function POST(req: NextRequest) {
+  try {
+    const { password, username } = await req.json()
+
+    await dbConnect()
+    const user = await User.findOne({ username })
+
+    const passwordCorrect =
+      user === null ? false : await bcrypt.compare(password, user.passwordHash)
+
+    if (!(user && passwordCorrect)) {
+      return NextResponse.json({
+        error: "Invalid User",
+        message: "Invalid username or password",
+        status: 400,
+      })
+    }
+
+    const userForToken = {
+      username: user.username,
+      id: user.__id,
+    }
+    if (SECRET) {
+      const token = jwt.sign(userForToken, SECRET, { expiresIn: 60 * 60 })
+
+      return NextResponse.json({
+        token,
+        username: user.username,
+        name: user.name,
+      })
+    } else {
+      return NextResponse.json({
+        error: "Missing Secret",
+        message: "SECRET missing from env",
+        status: 500,
+      })
+    }
+  } catch (error) {
+    ErrorHandler(error)
+  }
+}
