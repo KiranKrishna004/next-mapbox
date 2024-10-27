0f11e10910fe291314331d2c410f9d6d90adf876
Kiran Krishna N <kiran.krishna@nucash.money>
2024-10-28 03:00:53 +0530

    feat: add BASE_URL to env

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,5 +1,6 @@
 const URI = process.env.MONGODB_URI
 const SALT_ROUNDS = process.env.SALT_ROUNDS || 10
 const SECRET = process.env.SECRET
+const BASE_URL = process.env.NEXTAUTH_URL
 
-export { URI, SALT_ROUNDS, SECRET }
+export { URI, SALT_ROUNDS, SECRET, BASE_URL }
